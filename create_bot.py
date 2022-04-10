f71607182b274f75b9f0ba77ef3f1b7eeb71fc45
holmsya <iholmsya@gmail.com>
2022-04-10 16:28:32 +0300

    Fix token

--- create_bot.py
+++ create_bot.py
@@ -1,7 +1,7 @@
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage    # позволяет хранить данные в оперативной памяти
 
-API_TOKEN = '5191483081:AAF2Mf94vXaoMyA7kf7pZ2JpKGIYXf5_soc'
+API_TOKEN = ''
 
 storage = MemoryStorage()
 
